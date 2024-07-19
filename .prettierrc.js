9dedb9af77c11ec1d37d00af960af7144cfd5954
gobeomjun <theo@dwhale.kr>
2024-07-19 14:59:07 +0900

    s

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 120,
+    bracketSpacing: true,
+    endOfLine: "auto",
+    semi: true,
+    singleQuote: false,
+    trailingComma: "es5",
+    tabWidth: 2,
+    useTabs: false,
+};
